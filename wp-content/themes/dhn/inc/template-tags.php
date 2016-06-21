5c04795428e6055910d2c767c877343aa36ff015
traustid <traustid@gmail.com>
2016-06-21 09:41:45 +0200

    Display category name for posts.

--- wp-content/themes/dhn/inc/template-tags.php
+++ wp-content/themes/dhn/inc/template-tags.php
@@ -47,15 +47,15 @@ function dhn_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'dhn' ) );
-		if ( $categories_list && dhn_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'dhn' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		$categories_list = get_the_category_list( esc_html__( ' ', 'dhn' ) );
+		if ( $categories_list ) {
+			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'dhn' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ' ', 'dhn' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( '%1$s', 'dhn' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( ' <span class="tags-links">' . esc_html__( '%1$s', 'dhn' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 
